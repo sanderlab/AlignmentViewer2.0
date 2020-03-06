2466e59e5133b1306737be8c93cc121f12f0f9bf
Nick G <npgarbage@gmail.com>
2020-03-06 11:30:05 -0500

    Added single Alignment test & lcov

--- src/__tests__/Alignment.test.ts
+++ src/__tests__/Alignment.test.ts
@@ -1,6 +1,9 @@
-import Alignment from "../Alignment";
+import Alignment, { ISequence } from "../Alignment";
 
-describe("Alignment", () => {
+describe.only("Alignment", () => {
+  //let pse1TargetSequence: ISequence;
+  //let pse1Alignment: Alignment;
+
   it("Should allow sorting.", () => {
     const alignment = new Alignment("Test-Sequence", [
       { id: "id-1", sequence: "ZMA" }
@@ -31,4 +34,29 @@ describe("Alignment", () => {
     expected.set(3, { A: 1, C: 1 });
     expect(alignment.getPositionalLetterCounts()).toEqual(expected);
   });
+
+  it("Should return the first sequence in an alignment.", () => {
+    const seq1 = { id: "id-1", sequence: "ABC" };
+    const alignment = new Alignment("Test-Sequence", [
+      seq1,
+      { id: "id-2", sequence: "CBA" }
+    ]);
+    expect(alignment.getTargetSequence()).toEqual(seq1);
+  });
+
+  /*
+  beforeAll(async () => {
+    const result = await fetch(
+      `../public/7fa1c5691376beab198788a726917d48_b0.4.a2m`
+    );
+    pse1Alignment = Alignment.fromFile(
+      "7fa1c5691376beab198788a726917d48_b0.4.a2m",
+      await result.text()
+    );
+    pse1TargetSequence = pse1Alignment.getSequences()[0];
+    expect(pse1Alignment.getName()).toEqual(
+      "7fa1c5691376beab198788a726917d48_b0.4.a2m"
+    );
+  });
+  */
 });
