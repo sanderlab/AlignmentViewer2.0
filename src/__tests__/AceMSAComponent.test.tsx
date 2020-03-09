6ab5730dcaeddbfbb3dfddcc60b942e9d24025c9
Drew Diamantoukos <MercifulCode@gmail.com>
2020-03-09 13:13:24 -0400

    Test Side Story 2: Fetching Boogaloo (#2)
    * Setting up jest-fetch-mock
    
    * More tests for AceMSAComponent, AlignmentCanvasComponent, and ScrollSync. Added defaultProp of empty mouseHandler to AlignmentCanvasComponent.

--- src/__tests__/AceMSAComponent.test.tsx
+++ src/__tests__/AceMSAComponent.test.tsx
@@ -1,15 +1,49 @@
 import * as React from "react";
-import { shallow, default as Enzyme } from "enzyme";
+import { mount, shallow, default as Enzyme } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import "jest-canvas-mock";
 
 import Alignment from "../Alignment";
 import { AceMSAComponent, AceEditorTypes } from "../AceMSAComponent";
+import { Ace, default as ace } from "ace-builds";
+
+// Due to the runtime necessities of using styles, we need to explicitly mock out some stub data.
+// https://github.com/facebook/jest/issues/3094
+jest.mock("../MolecularStyles.module.scss", () => {
+  return {
+    aaStyBGAlpha_Default: "",
+    aaStyClass_Default: "",
+    aaStyColorOrder_Default: "",
+    aaStyColors_Default: "",
+    aaStyDesc_Default: "",
+    ntStyBGAlpha_Default: "",
+    ntStyClass_Default: "",
+    ntStyColorOrder_Default: "",
+    ntStyColors_Default: "",
+    ntStyDesc_Default: ""
+  };
+});
 
 describe("AceMSAComponent", () => {
+  let editor: Ace.Editor;
+  let editorLoadedSpy: jest.Mock;
+
   beforeAll(() => {
     Enzyme.configure({ adapter: new Adapter() });
   });
+
+  beforeEach(() => {
+    jest.clearAllTimers();
+    jest.useFakeTimers();
+    editorLoadedSpy = jest.fn(e => {
+      expect(e).not.toBeUndefined();
+      expect(e).not.toBeNull();
+      editor = ace.edit(e);
+      expect(editor).not.toBeUndefined();
+      expect(editor).not.toBeNull();
+    });
+  });
+
   it("Should render when given default props.", () => {
     const wrapper = shallow(
       <AceMSAComponent
@@ -23,20 +57,63 @@ describe("AceMSAComponent", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it.each([0, 1, 2, 3])(
-    "Should render for an ace editor of type '%i'",
-    editor => {
-      console.log(editor);
-      const wrapper = shallow(
-        <AceMSAComponent
-          editorLoaded={jest.fn()}
-          alignment={new Alignment("", [])}
-          fontSize={4}
-          id={"ace-msa-editor"}
-          type={editor}
-        />
-      );
-      expect(wrapper).toMatchSnapshot();
-    }
-  );
+  it("Should invoke the loaded callback when the Ace editor is loaded.", () => {
+    mount(
+      <AceMSAComponent
+        editorLoaded={editorLoadedSpy}
+        alignment={
+          new Alignment("test-alignment", [{ id: "Psychic", sequence: "ABRA" }])
+        }
+        fontSize={4}
+        id={"ace-msa-editor"}
+        type={AceEditorTypes.alignment}
+      />
+    );
+    expect(editorLoadedSpy).toHaveBeenCalled();
+
+    expect(editorLoadedSpy.mock.calls[0][0]).toEqual(editor);
+  });
+
+  it("Should correctly generate a Textual Ruler for when there are no characters.", () => {
+    expect(AceMSAComponent.generateTextualRuler(0)).toEqual(".");
+  });
+
+  it("Should correctly generate a empty Textual Ruler when there are negative characters.", () => {
+    expect(AceMSAComponent.generateTextualRuler(-1)).toEqual("");
+  });
+
+  it("Should correctly generate a Textual Ruler when there are some characters.", () => {
+    let expected = "..";
+    expect(AceMSAComponent.generateTextualRuler(1)).toEqual(expected);
+
+    expected = "...";
+    expect(AceMSAComponent.generateTextualRuler(2)).toEqual(expected);
+
+    expected = "....:.";
+    expect(AceMSAComponent.generateTextualRuler(5)).toEqual(expected);
+
+    expected = "....:..10|.";
+    expect(AceMSAComponent.generateTextualRuler(10)).toEqual(expected);
+
+    expected = "....:..10|....:..20|.";
+    expect(AceMSAComponent.generateTextualRuler(20)).toEqual(expected);
+  });
+
+  it.each([
+    AceEditorTypes.alignment,
+    AceEditorTypes.consensus,
+    AceEditorTypes.position,
+    AceEditorTypes.query
+  ])("Should render for an ace editor of type '%i'", editor => {
+    const wrapper = shallow(
+      <AceMSAComponent
+        editorLoaded={jest.fn()}
+        alignment={new Alignment("", [])}
+        fontSize={4}
+        id={"ace-msa-editor"}
+        type={editor}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
 });
--- src/__tests__/ScrollSync.test.tsx
+++ src/__tests__/ScrollSync.test.tsx
@@ -1,9 +1,42 @@
-import ScrollSync from "../ScrollSync";
+import * as React from "react";
+import { mount, default as Enzyme } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import ScrollSync, { ScrollType } from "../ScrollSync";
 
 describe("ScrollSync", () => {
+  beforeAll(() => {
+    Enzyme.configure({ adapter: new Adapter() });
+  });
+
   it("Should work.", () => {
     const scrollSyncInstance = ScrollSync.getInstance();
     expect(scrollSyncInstance).not.toBeUndefined();
     expect(scrollSyncInstance).not.toBeNull();
   });
+
+  it.each([ScrollType.both, ScrollType.horizontal, ScrollType.vertical])(
+    "Should allow registering a scroller of type %s to a scroll group.",
+    scrollType => {
+      const wrapper = mount(<div />);
+      const scrollSyncInstance = ScrollSync.getInstance();
+      scrollSyncInstance.setScrollerGroup("cool-kids", scrollType);
+      scrollSyncInstance.registerScroller(
+        wrapper.getDOMNode() as HTMLElement,
+        "cool-kids"
+      );
+
+      wrapper
+        .getDOMNode()
+        .dispatchEvent(new window.UIEvent("scroll", { detail: 0 }));
+      wrapper.update();
+    }
+  );
+
+  it.each([ScrollType.both, ScrollType.horizontal, ScrollType.vertical])(
+    "Should allow setting a scroller of type %s to a scroll group.",
+    scrollType => {
+      const scrollSyncInstance = ScrollSync.getInstance();
+      scrollSyncInstance.setScrollerGroup("cooler-kids", scrollType);
+    }
+  );
 });
