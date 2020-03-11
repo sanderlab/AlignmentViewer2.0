f2559107ac67d34293711dce5ce7a8603be34795
Drew Diamantoukos <MercifulCode@gmail.com>
2020-03-11 14:47:12 -0400

    Last Living Souls (#5)
    * Split up the render methods in App, AlignmentViewer and AlignmentCanvasComponent into smaller functions. Added test for App to verify changes introduced no regressions.
    
    * updated molecule function for clarity
    
    * fixed bug from last commit
    
    Co-authored-by: npgauth <npgarbage@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -48,209 +48,258 @@ export default class App extends React.Component<AppProps, AppState> {
   }
 
   render() {
-    return !this.state || !this.state.alignment || !this.state.style ? null : (
+    const { alignment, logoPlotStyle, sortBy, style, zoomLevel } = this.state;
+    return !alignment || !style ? null : (
       <div>
-        <div className="settings_box">
-          <form>
-            <div>
-              <h3>AlignmentViewer 2.0 Settings Demo</h3>
-            </div>
-
-            <div>
-              <label>
-                <strong>Sort order:</strong>
-                <select
-                  value={this.state.sortBy.key}
-                  onChange={e =>
-                    this.setState({
-                      sortBy: SequenceSortOptions.fromKey(e.target.value)!
-                    })
-                  }
-                >
-                  {SequenceSortOptions.list.map(sso => {
-                    return (
-                      <option value={sso.key} key={sso.key}>
-                        {sso.description}
-                      </option>
-                    );
-                  })}
-                </select>
-              </label>
-            </div>
-
-            <div>
-              <label>
-                <strong>Alignment Type:</strong>
-                <select
-                  value={this.state.style.alignmentType.key}
-                  onChange={e =>
-                    this.setState({
-                      style: AlignmentStyle.fromAlignmentType(
-                        AlignmentTypes.fromKey(e.target.value)!
-                      )
-                    })
-                  }
-                >
-                  {AlignmentTypes.list.map(alignmentType => {
-                    return (
-                      <option value={alignmentType.key} key={alignmentType.key}>
-                        {alignmentType.description}
-                      </option>
-                    );
-                  })}
-                </select>
-              </label>
-            </div>
-            <div>
-              <label>
-                <strong>Style:</strong>
-                <select
-                  value={this.state.style.alignmentType.allColorSchemes.indexOf(
-                    this.state.style.colorScheme
-                  )}
-                  onChange={e => {
-                    this.setState({
-                      style: {
-                        ...this.state.style!,
-                        colorScheme: this.state.style!.alignmentType
-                          .allColorSchemes[parseInt(e.target.value)]
-                      }
-                    });
-                  }}
-                >
-                  {this.state.style.alignmentType.allColorSchemes.map(
-                    (colorScheme: IColorScheme, index: number) => {
-                      return (
-                        <option key={index} value={index}>
-                          {colorScheme.description}
-                        </option>
-                      );
-                    }
-                  )}
-                </select>
-              </label>
-            </div>
-            <div>
-              <label>
-                <strong>Residue Detail:</strong>
-                <select
-                  value={this.state.style.residueDetail.key}
-                  onChange={e => {
-                    this.setState({
-                      style: {
-                        ...this.state.style!,
-                        residueDetail: ResidueDetailTypes.fromKey(
-                          e.target.value
-                        )!
-                      }
-                    });
-                  }}
-                >
-                  {ResidueDetailTypes.list.map(rd => {
-                    return (
-                      <option key={rd.key} value={rd.key}>
-                        {rd.description}
-                      </option>
-                    );
-                  })}
-                </select>
-              </label>
-            </div>
-            <div>
-              <label>
-                <strong>Positions to Style:</strong>
-                <select
-                  value={PositionsToStyle.list.indexOf(
-                    this.state.style.positionsToStyle
-                  )}
-                  onChange={e => {
-                    this.setState({
-                      style: {
-                        ...this.state.style!,
-                        positionsToStyle:
-                          PositionsToStyle.list[parseInt(e.target.value)]
-                      }
-                    });
-                  }}
-                >
-                  {PositionsToStyle.list.map(
-                    (pts: PositionsToStyle, index: number) => {
-                      return (
-                        <option key={index} value={index}>
-                          {pts.description}
-                        </option>
-                      );
-                    }
-                  )}
-                </select>
-              </label>
-            </div>
-
-            <div>
-              <label>
-                <strong>Sequence Logo Style:</strong>
-                <select
-                  value={this.state.logoPlotStyle}
-                  onChange={e => {
-                    this.setState({
-                      logoPlotStyle: e.target.value as LOGO_TYPES
-                    });
-                  }}
-                >
-                  {Object.values(LOGO_TYPES).map(logoType => {
-                    return (
-                      <option key={logoType} value={logoType}>
-                        {logoType}
-                      </option>
-                    );
-                  })}
-                </select>
-              </label>
-            </div>
-
-            <div>
-              <label>
-                <strong>Zoom:</strong>
-                <div className="zoom-level">
-                  <button
-                    type="button"
-                    disabled={this.state.zoomLevel < 3}
-                    onClick={e => {
-                      this.setState({
-                        zoomLevel: this.state.zoomLevel - 1
-                      });
-                    }}
-                  >
-                    -
-                  </button>
-                  {this.state.zoomLevel}
-                  <button
-                    type="button"
-                    disabled={this.state.zoomLevel > 15}
-                    onClick={e => {
-                      this.setState({
-                        zoomLevel: this.state.zoomLevel + 1
-                      });
-                    }}
-                  >
-                    +
-                  </button>
-                </div>
-              </label>
-            </div>
-          </form>
-        </div>
-
+        {this.renderSettingsBox(style)}
         <div className="av_holder">
           <AlignmentViewer
-            alignment={this.state.alignment}
-            style={this.state.style}
-            logoPlotStyle={this.state.logoPlotStyle}
-            zoomLevel={this.state.zoomLevel}
-            sortBy={this.state.sortBy}
+            alignment={alignment}
+            style={style}
+            logoPlotStyle={logoPlotStyle}
+            zoomLevel={zoomLevel}
+            sortBy={sortBy}
           ></AlignmentViewer>
         </div>
       </div>
     );
   }
+
+  protected renderSettingsBox = (
+    style: AminoAcidAlignmentStyle | NucleotideAlignmentStyle
+  ) => {
+    return (
+      <div className="settings_box">
+        <form>
+          <div>
+            <h3>AlignmentViewer 2.0 Settings Demo</h3>
+          </div>
+          {this.renderSortControl()}
+          {this.renderAlignmentTypeLabel(style)}
+          {this.renderColorScheme(style)}
+          {this.renderResidueDetail(style)}
+          {this.renderPositionStyling(style)}
+          {this.renderSequenceLogo()}
+          {this.renderZoomButtons()}
+        </form>
+      </div>
+    );
+  };
+
+  protected renderSortControl = () => {
+    const { sortBy } = this.state;
+    return (
+      <div>
+        <label>
+          <strong>Sort order:</strong>
+          <select
+            value={sortBy.key}
+            onChange={e =>
+              this.setState({
+                sortBy: SequenceSortOptions.fromKey(e.target.value)!
+              })
+            }
+          >
+            {SequenceSortOptions.list.map(sso => {
+              return (
+                <option value={sso.key} key={sso.key}>
+                  {sso.description}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+      </div>
+    );
+  };
+  protected renderAlignmentTypeLabel = (
+    style: AminoAcidAlignmentStyle | NucleotideAlignmentStyle
+  ) => {
+    return (
+      <div>
+        <label>
+          <strong>Alignment Type:</strong>
+          <select
+            value={style.alignmentType.key}
+            onChange={e =>
+              this.setState({
+                style: AlignmentStyle.fromAlignmentType(
+                  AlignmentTypes.fromKey(e.target.value)!
+                )
+              })
+            }
+          >
+            {AlignmentTypes.list.map(alignmentType => {
+              return (
+                <option value={alignmentType.key} key={alignmentType.key}>
+                  {alignmentType.description}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+      </div>
+    );
+  };
+  protected renderColorScheme = (
+    style: AminoAcidAlignmentStyle | NucleotideAlignmentStyle
+  ) => {
+    return (
+      <div>
+        <label>
+          <strong>Style:</strong>
+          <select
+            value={style.alignmentType.allColorSchemes.indexOf(
+              style.colorScheme
+            )}
+            onChange={e => {
+              this.setState({
+                style: {
+                  ...style!,
+                  colorScheme:
+                    style.alignmentType.allColorSchemes[
+                      parseInt(e.target.value)
+                    ]
+                }
+              });
+            }}
+          >
+            {style.alignmentType.allColorSchemes.map(
+              (colorScheme: IColorScheme, index: number) => {
+                return (
+                  <option key={index} value={index}>
+                    {colorScheme.description}
+                  </option>
+                );
+              }
+            )}
+          </select>
+        </label>
+      </div>
+    );
+  };
+  protected renderResidueDetail = (
+    style: AminoAcidAlignmentStyle | NucleotideAlignmentStyle
+  ) => {
+    return (
+      <div>
+        <label>
+          <strong>Residue Detail:</strong>
+          <select
+            value={style.residueDetail.key}
+            onChange={e => {
+              this.setState({
+                style: {
+                  ...style,
+                  residueDetail: ResidueDetailTypes.fromKey(e.target.value)!
+                }
+              });
+            }}
+          >
+            {ResidueDetailTypes.list.map(rd => {
+              return (
+                <option key={rd.key} value={rd.key}>
+                  {rd.description}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+      </div>
+    );
+  };
+  protected renderPositionStyling = (
+    style: AminoAcidAlignmentStyle | NucleotideAlignmentStyle
+  ) => {
+    return (
+      <div>
+        <label>
+          <strong>Positions to Style:</strong>
+          <select
+            value={PositionsToStyle.list.indexOf(style.positionsToStyle)}
+            onChange={e => {
+              this.setState({
+                style: {
+                  ...style,
+                  positionsToStyle:
+                    PositionsToStyle.list[parseInt(e.target.value)]
+                }
+              });
+            }}
+          >
+            {PositionsToStyle.list.map(
+              (pts: PositionsToStyle, index: number) => {
+                return (
+                  <option key={index} value={index}>
+                    {pts.description}
+                  </option>
+                );
+              }
+            )}
+          </select>
+        </label>
+      </div>
+    );
+  };
+  protected renderSequenceLogo = () => {
+    const { logoPlotStyle } = this.state;
+    return (
+      <div>
+        <label>
+          <strong>Sequence Logo Style:</strong>
+          <select
+            value={logoPlotStyle}
+            onChange={e => {
+              this.setState({
+                logoPlotStyle: e.target.value as LOGO_TYPES
+              });
+            }}
+          >
+            {Object.values(LOGO_TYPES).map(logoType => {
+              return (
+                <option key={logoType} value={logoType}>
+                  {logoType}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+      </div>
+    );
+  };
+  protected renderZoomButtons = () => {
+    const { zoomLevel } = this.state;
+    return (
+      <div>
+        <label>
+          <strong>Zoom:</strong>
+          <div className="zoom-level">
+            <button
+              type="button"
+              disabled={zoomLevel < 3}
+              onClick={e => {
+                this.setState({
+                  zoomLevel: zoomLevel - 1
+                });
+              }}
+            >
+              -
+            </button>
+            {zoomLevel}
+            <button
+              type="button"
+              disabled={zoomLevel > 15}
+              onClick={e => {
+                this.setState({
+                  zoomLevel: zoomLevel + 1
+                });
+              }}
+            >
+              +
+            </button>
+          </div>
+        </label>
+      </div>
+    );
+  };
 }
